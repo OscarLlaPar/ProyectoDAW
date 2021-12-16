054339808385d6caad618a402749a5f8eb787cdf
Oscar <oscar.llapar@educa.jcyl.es>
2021-12-16 01:36:09 +0100

    15/12/2021: Enlace al repositorio Panel de información del desarrollador

--- index.php
+++ index.php
@@ -11,31 +11,45 @@ and open the template in the editor.
         <link href="webroot/css/estilos.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-            <div class="titulo">
-                <h1>Desarrollo de Aplicaciones Web</h1>
-                <h2>Óscar Llamas Parra</h2>
-                <div class="cuadro" id="arriba"></div>
-            </div>
-        <div class="cont">
+        <div class="titulo">
+            <h1>Desarrollo de Aplicaciones Web</h1>
+            <h2>Óscar Llamas Parra</h2>
+            <div class="cuadro" id="arriba"></div>
+        </div>
+        <main>
+            <div class="cont">
                 <div class="asig" id="DAW"><p class="sigla">DAW</p><p>Despliegue de Aplicaciones Web</p></div>
                 <div class="asig" id="DIW"><p class="sigla">DIW</p><p>Desarrollo de Interfaces Web</p></div>
-                <div class="asig" id="DWEC"><a href="ProyectoDWEC/indexProyectoDWEC.php"><p class="sigla">DWEC</p></a><p>Deasrrollo Web en Entorno Cliente</p></div>
+                <div class="asig" id="DWEC"><p class="sigla">DWEC</p></a><p>Deasrrollo Web en Entorno Cliente</div>
                 <div class="asig" id="DWES"><a href="ProyectoDWES/indexProyectoDWES.php"><p class="sigla">DWES</p></a><p>Desarrollo Web en Entorno Servidor</p></div>
                 <div class="asig" id="EIE"><p class="sigla">EIE</p><p>Empresa e Iniciativa Emprendedora</p></div>
-        </div>
-			
+            </div>
+            <div class="info">
+                <h3>Óscar Llamas</h3>
+                <p>Estudiante de desarrollo web - DAW 2.</p>
+                <p>Lenguajes conocidos:</p>
+                <ul>
+                    <li>PHP</li>
+                    <li>Java</li>
+                    <li>MySQL</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                </ul>
+            </div>	
+        </main>
+        <footer>
+            <p>
+                Óscar Llamas Parra
+                <a href="https://github.com/OscarLlaPar/proyectoDAW" target="__blank"><img src="webroot/img/github.png" alt="Github"></img></a>
+            </p>
+            <p>
+                DAW 2
+            </p>
+            <p>
+                IES Los Sauces, Benavente 2021-2022
+            </p>
+            <div class="cuadro" id="abajo"></div>
+        </footer>
     </body>
-    <footer>
-        <p>
-            Óscar Llamas Parra
-			<a href="https://github.com/OscarLlaPar/" target="__blank"><img src="webroot/img/github.png" alt="Github"></img></a>
-        </p>
-        <p>
-            DAW 2
-        </p>
-        <p>
-            IES Los Sauces, Benavente 2021-2022
-        </p>
-        <div class="cuadro" id="abajo"></div>
-    </footer>
 </html>
